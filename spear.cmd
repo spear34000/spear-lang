86887bfefc053789124b0d556dd7569e4da48b94
spear34000 <limchrtygif2@gmail.com>
2026-03-18 23:59:07 +0900

    Make local spear.cmd forward to current CLI

--- spear.cmd
+++ spear.cmd
@@ -1,96 +1,15 @@
 @echo off
 setlocal
 
-if "%~1"=="" goto :usage
-
-set "mode=run"
-set "input=%~1"
-
-if /I "%~1"=="build" (
-    if "%~2"=="" goto :usage
-    set "mode=build"
-    set "input=%~2"
-)
-
-if /I "%~1"=="serve" (
-    if "%~2"=="" goto :usage
-    set "mode=serve"
-    set "input=%~2"
-)
-
-if /I "%~1"=="service" (
-    if "%~2"=="" goto :usage
-    set "mode=serve"
-    set "input=%~2"
-)
-
-if /I "%~1"=="check" (
-    if "%~2"=="" goto :usage
-    set "mode=check"
-    set "input=%~2"
-)
-
-if not exist "%input%" (
-    echo input not found: %input%
-    exit /b 1
-)
-
-if /I not "%input:~-3%"==".sp" (
-    echo expected a .sp source file
-    exit /b 1
-)
-
-if not exist "build" mkdir build
-
-if not exist "build\spearc.exe" (
-    gcc -O3 -Wall -Wextra -std=c11 -o build\spearc.exe src\spearc.c
-    if errorlevel 1 exit /b 1
-)
-
-if /I "%mode%"=="check" (
-    build\spearc.exe --check "%input%"
-    if errorlevel 1 exit /b 1
-    echo checked %input%
-    exit /b 0
-)
-
-set "name=%~n1"
-if /I "%mode%"=="build" set "name=%~n2"
-if /I "%mode%"=="serve" set "name=%~n2"
-
-set "c_out=build\%name%.c"
-set "exe_out=build\%name%.exe"
-
-build\spearc.exe "%input%" -o "%c_out%"
-if errorlevel 1 exit /b 1
-
-gcc -O3 -Wall -Wextra -std=c11 -o "%exe_out%" "%c_out%"
-if errorlevel 1 exit /b 1
-
-if /I "%mode%"=="build" (
-    echo built %exe_out%
-    exit /b 0
-)
-
-if /I "%mode%"=="serve" (
-    "%exe_out%"
-    if errorlevel 1 exit /b 1
-    echo serving build at http://127.0.0.1:4173/
-    if exist "build\spear-ui.html" (
-        echo page: http://127.0.0.1:4173/spear-ui.html
-    )
-    python -m http.server 4173 --directory build
+if exist "%~dp0build\spear.exe" (
+    "%~dp0build\spear.exe" %*
     exit /b %errorlevel%
 )
 
-"%exe_out%"
-exit /b %errorlevel%
+if exist "%LOCALAPPDATA%\Programs\Spear\bin\spear.exe" (
+    "%LOCALAPPDATA%\Programs\Spear\bin\spear.exe" %*
+    exit /b %errorlevel%
+)
 
-:usage
-echo usage:
-echo   spear file.sp
-echo   spear build file.sp
-echo   spear serve file.sp
-echo   spear service file.sp
-echo   spear check file.sp
+echo spear error: could not find a built or installed spear.exe
 exit /b 1
